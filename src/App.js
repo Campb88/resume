f3157be602826c58eec2521fb377a6618e085914
Campb88 <campb88@wwu.edu>
2023-11-26 12:42:55 -0800

    with theme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ReactGA from "react-ga";
+import "./css/layout.css";
+import Header from "./Components/Header.js";
+/* import About from "./Components/About";
+import Resume from "./Components/Resume";
+import Portfolio from "./Components/Portfolio";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer"; */
+import resumeData from "./resumeData.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      foo: "bar",
+      resumeData: {}
+    };
+
+    ReactGA.initialize("UA-110570651-1");
+    ReactGA.pageview(window.location.pathname);
+  }
+
+  componentDidMount() {
+    // You may not need this function anymore
+    // If resumeData.json is static and doesn't change, you can directly use `resumeData` variable.
+    // If it does change dynamically, you might still want to fetch it dynamically.
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header data={resumeData.main} />
+{/*         <About data={resumeData.main} />
+        <Resume data={resumeData.resume} />
+        <Portfolio data={resumeData.portfolio} />
+        <Contact data={resumeData.main} />
+        <Footer data={resumeData.main} /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
