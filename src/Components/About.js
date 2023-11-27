756842a9c5bf335344ca1ced02dba1c82e62a8b9
Kate Campbell <campb88@cf404-hrd-01.cs.wwu.edu>
2023-11-27 13:01:39 -0800

    added mission statements, centered content

--- src/Components/About.js
+++ src/Components/About.js
@@ -12,6 +12,7 @@ class About extends Component {
         const state = this.props.data.address.state;
         const zip = this.props.data.address.zip;
         const email = this.props.data.email;
+        const professional = this.props.data.professional;
         /* const resumeDownload = "../docs/Resume.pdf";
  */
         return (
@@ -25,12 +26,12 @@ class About extends Component {
                         />
                     </div>
                     <div className="about-details">
-                        <h2>About Me</h2>
+                        <h2>ABOUT ME</h2>
                         <p>{bio}</p>
                         <p>{bio2}</p>
                     </div>
                     <div className="contact-details">
-                        <h2>Contact Details</h2>
+                        <h2>CONTACT DETAILS</h2>
                         <p className="address">
                             <span>{name}</span>
                             <br />
@@ -40,16 +41,36 @@ class About extends Component {
                             <br />
                             <span>{email}</span>
                         </p>
+                        <div className="download">
+                            <p>
+                                <a target="_blank" rel="noopener noreferrer" href="https://docs.google.com/document/d/e/2PACX-1vSgXKrPhTqEoaMmZVmVIbiBuD_17ADLscjt8qSdOUOZx0bNQuXfqsu8rnCGe8_F1zRqXcSbKkTeRrD7/pub" className="button">
+                                    <i className="fas fa-download"></i>Download Resume
+                                </a>
+                            </p>
+                        </div>
                     </div>
-                    <div className="download">
-                        <p>
-                            <a target="_blank" rel="noopener noreferrer" href="https://www.google.com" className="button">
-                                <i className="fas fa-download"></i>Download Resume
-                            </a>
 
+
+                    <div className="prof-mission">
+                        <h2>PROFESSIONAL MISSION:</h2>
+                        <p className="professional">
+                            <span>{professional}</span>
+                        </p>
+                    </div>
+                    <div className="pers-mission">
+                        <h2>PROFESSIONAL MISSION:</h2>
+                        <p className="personal">
+                            <span>{professional}</span>
                         </p>
 
                     </div>
+
+
+
+
+
+
+
                 </div>
             </section>
         );
